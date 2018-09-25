0002f10d61e61d07e8402af1d602a871204174ba
CyrusNajmabadi <cyrus.najmabadi@gmail.com>
2018-09-25 13:51:26 -0700

    Swithch to the 'dev' version of our dependencies. (#124)

--- resources.go
+++ resources.go
@@ -581,7 +581,7 @@ func Provider() tfbridge.ProviderInfo {
 				"@types/node": "^8.0.25", // so we can access strongly typed node definitions.
 			},
 			Dependencies: map[string]string{
-				"@pulumi/pulumi": "^0.15.3-dev",
+				"@pulumi/pulumi": "dev",
 			},
 			Overlay: &tfbridge.OverlayInfo{
 				Files:   []string{},
