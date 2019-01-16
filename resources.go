f64feaf049dfb0ddb03832b663ba8946f9fc4865
Cyrus Najmabadi <cyrus@pulumi.com>
2019-01-16 13:27:34 -0800

    fixup things

--- resources.go
+++ resources.go
@@ -673,7 +673,7 @@ func Provider() tfbridge.ProviderInfo {
 		},
 		JavaScript: &tfbridge.JavaScriptInfo{
 			DevDependencies: map[string]string{
-				"@types/node": "^8.0.25", // so we can access strongly typed node definitions.
+				"@types/node": "^10.0.0", // so we can access strongly typed node definitions.
 			},
 			Dependencies: map[string]string{
 				"@pulumi/pulumi": "^0.16.10",
