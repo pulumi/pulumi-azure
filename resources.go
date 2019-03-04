f6dd26c0c8df94e2e7917ba54f8a0e97e0d97691
Cyrus Najmabadi <cyrus.najmabadi@gmail.com>
2019-03-04 15:53:02 -0800

    flow downstream

--- resources.go
+++ resources.go
@@ -713,7 +713,7 @@ func Provider() tfbridge.ProviderInfo {
 				"@types/node": "^10.0.0", // so we can access strongly typed node definitions.
 			},
 			Dependencies: map[string]string{
-				"@pulumi/pulumi": "^0.16.17",
+				"@pulumi/pulumi": "feature-seventeen",
 			},
 			Overlay: &tfbridge.OverlayInfo{
 				Files:   []string{},
