2cae1efccafb4eda324091cf75f43a4f812d40bd
Mikhail Shilkov <shilkov@gmail.com>
2019-06-12 11:08:18 +0200

    Fix the example

--- examples/http-external/index.ts
+++ examples/http-external/index.ts
@@ -4,7 +4,7 @@ import * as azure from "@pulumi/azure";
 const resourceGroup = new azure.core.ResourceGroup('example', { location: 'West US 2' });
 
 // Create a Function App implemented in PowerShell with source code from 'funcapp' folder
-const app = new azure.appservice.MultiFunctionApp("http-ps", {
+const app = new azure.appservice.ArchiveFunctionApp("http-ps", {
     resourceGroupName: resourceGroup.name,
     archive: new pulumi.asset.FileArchive("./funcapp"),
     appSettings: {
