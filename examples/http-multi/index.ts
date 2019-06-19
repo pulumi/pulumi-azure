10e5466ecdae5e6e61c8d1696ffdeeb19ae8179b
Mikhail Shilkov <shilkov@gmail.com>
2019-06-19 10:02:54 +0200

    Formatting

--- examples/http-multi/index.ts
+++ examples/http-multi/index.ts
@@ -3,15 +3,15 @@ import * as azure from "@pulumi/azure";
 const resourceGroup = new azure.core.ResourceGroup("example", { location: azure.Locations.WestUS2 });
 
 // Define 3 HTTP Functions which only differ by number and the hello message
-const http = [1, 2, 3].map(i => 
-    new azure.appservice.HttpFunction(`F${i}`, { 
-        callback: async (context, request) => ({ status: 200, body: `Hi from F${i}` }) 
+const http = [1, 2, 3].map(i =>
+    new azure.appservice.HttpFunction(`F${i}`, {
+        callback: async (context, request) => ({ status: 200, body: `Hi from F${i}` }),
     }),
 );
 
 // Define a timer function which will trigger every minute to keep other Function from being disposed on idle
 const warmer = new azure.appservice.TimerFunction("Warmer", {
-    schedule: { second: 0 }, 
+    schedule: { second: 0 },
     callback: async (context, timer) => {
         // Do nothing, it's just a warmer
     },
