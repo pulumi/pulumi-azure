35b19ccd7d0fd7db63263455243d172c93aef764
stack72 <public@paulstack.co.uk>
2019-10-11 18:24:17 +0300

    Examples: Change the examples to run as individual tests

--- examples/examples_test.go
+++ examples/examples_test.go
@@ -17,181 +17,335 @@ import (
 	"github.com/pulumi/pulumi/pkg/testing/integration"
 )
 
-func TestExamples(t *testing.T) {
-	environ := os.Getenv("ARM_ENVIRONMENT")
-	if environ == "" {
-		t.Skipf("Skipping test due to missing ARM_ENVIRONMENT variable")
+func TestAccMinimal(t *testing.T) {
+	test := getJSBaseOptions(t).
+		With(integration.ProgramTestOptions{
+			Dir: path.Join(getCwd(t), "minimal"),
+		})
+
+	integration.ProgramTest(t, &test)
+}
+
+func TestAccEventgrid(t *testing.T) {
+	test := getJSBaseOptions(t).
+		With(integration.ProgramTestOptions{
+			Dir:           path.Join(getCwd(t), "eventgrid"),
+			RunUpdateTest: true,
+		})
+
+	integration.ProgramTest(t, &test)
+}
+
+func TestAccEventhub(t *testing.T) {
+	test := getJSBaseOptions(t).
+		With(integration.ProgramTestOptions{
+			Dir:           path.Join(getCwd(t), "eventhub"),
+			RunUpdateTest: true,
+			// work around https://github.com/terraform-providers/terraform-provider-azurerm/issues/4598
+			AllowEmptyPreviewChanges: true,
+			AllowEmptyUpdateChanges:  true,
+		})
+
+	integration.ProgramTest(t, &test)
+}
+
+func TestAccHttpExternal(t *testing.T) {
+	test := getJSBaseOptions(t).
+		With(integration.ProgramTestOptions{
+			Dir:           path.Join(getCwd(t), "http-external"),
+			RunUpdateTest: true,
+		})
+
+	integration.ProgramTest(t, &test)
+}
+
+func TestAccHttpMulti(t *testing.T) {
+	test := getJSBaseOptions(t).
+		With(integration.ProgramTestOptions{
+			Dir:           path.Join(getCwd(t), "http-multi"),
+			RunUpdateTest: true,
+			// work around https://github.com/terraform-providers/terraform-provider-azurerm/issues/4598
+			AllowEmptyPreviewChanges: true,
+			AllowEmptyUpdateChanges:  true,
+		})
+
+	integration.ProgramTest(t, &test)
+}
+
+func TestAccIot(t *testing.T) {
+	test := getJSBaseOptions(t).
+		With(integration.ProgramTestOptions{
+			Dir:           path.Join(getCwd(t), "iot"),
+			RunUpdateTest: true,
+		})
+
+	integration.ProgramTest(t, &test)
+}
+
+func TestAccQueue(t *testing.T) {
+	test := getJSBaseOptions(t).
+		With(integration.ProgramTestOptions{
+			Dir:           path.Join(getCwd(t), "queue"),
+			RunUpdateTest: true,
+		})
+
+	integration.ProgramTest(t, &test)
+}
+
+func TestAccTimer(t *testing.T) {
+	test := getJSBaseOptions(t).
+		With(integration.ProgramTestOptions{
+			Dir:           path.Join(getCwd(t), "timer"),
+			RunUpdateTest: true,
+		})
+
+	integration.ProgramTest(t, &test)
+}
+
+func TestAccTopic(t *testing.T) {
+	test := getJSBaseOptions(t).
+		With(integration.ProgramTestOptions{
+			Dir:           path.Join(getCwd(t), "topic"),
+			RunUpdateTest: true,
+		})
+
+	integration.ProgramTest(t, &test)
+}
+
+func TestAccWebserver(t *testing.T) {
+	test := getJSBaseOptions(t).
+		With(integration.ProgramTestOptions{
+			Dir:                  path.Join(getCwd(t), "webserver"),
+			RunUpdateTest:        true,
+			ExpectRefreshChanges: true,
+		})
+
+	integration.ProgramTest(t, &test)
+}
+
+func TestAccHttp(t *testing.T) {
+	test := getJSBaseOptions(t).
+		With(integration.ProgramTestOptions{
+			Dir:           path.Join(getCwd(t), "http"),
+			RunUpdateTest: true,
+			ExtraRuntimeValidation: validateAPITest(func(body string) {
+				assert.Equal(t, body, "Hello World!")
+			}),
+		})
+
+	integration.ProgramTest(t, &test)
+}
+
+func TestAccBlob(t *testing.T) {
+	test := getJSBaseOptions(t).
+		With(integration.ProgramTestOptions{
+			Dir:           path.Join(getCwd(t), "blob"),
+			RunUpdateTest: true,
+			ExtraRuntimeValidation: validateAPITest(func(body string) {
+				assert.Equal(t, body, "A File from Blob Storage")
+			}),
+		})
+
+	integration.ProgramTest(t, &test)
+}
+
+func TestAccMsiRenamed(t *testing.T) {
+	test := getJSBaseOptions(t).
+		With(integration.ProgramTestOptions{
+			Dir:           path.Join(getCwd(t), "msi-renamed-to-authorization"),
+			RunUpdateTest: true,
+			EditDirs: []integration.EditDir{
+				{
+					Dir:             "step2",
+					Additive:        true,
+					ExpectNoChanges: true,
+				},
+			},
+		})
+
+	integration.ProgramTest(t, &test)
+}
+
+func TestAccServicebusMigration(t *testing.T) {
+	test := getJSBaseOptions(t).
+		With(integration.ProgramTestOptions{
+			Dir:           path.Join(getCwd(t), "servicebus-migration-test"),
+			RunUpdateTest: true,
+			EditDirs: []integration.EditDir{
+				{
+					Dir:             "step2",
+					Additive:        true,
+					ExpectNoChanges: true,
+				},
+			},
+		})
+
+	integration.ProgramTest(t, &test)
+}
+
+func TestAccTable(t *testing.T) {
+	test := getJSBaseOptions(t).
+		With(integration.ProgramTestOptions{
+			Dir:           path.Join(getCwd(t), "table"),
+			RunUpdateTest: true,
+		})
+
+	integration.ProgramTest(t, &test)
+}
+
+func TestAccMinimalPy(t *testing.T) {
+	test := getPythonBaseOptions(t).
+		With(integration.ProgramTestOptions{
+			Dir: path.Join(getCwd(t), "minimal-py"),
+		})
+
+	integration.ProgramTest(t, &test)
+}
+
+func TestAccEventhubPy(t *testing.T) {
+	test := getPythonBaseOptions(t).
+		With(integration.ProgramTestOptions{
+			Dir:           path.Join(getCwd(t), "eventhub-py"),
+			RunUpdateTest: true,
+			// work around https://github.com/terraform-providers/terraform-provider-azurerm/issues/4598
+			AllowEmptyPreviewChanges: true,
+			AllowEmptyUpdateChanges:  true,
+		})
+
+	integration.ProgramTest(t, &test)
+}
+
+func TestAccDatasourcePy(t *testing.T) {
+	test := getPythonBaseOptions(t).
+		With(integration.ProgramTestOptions{
+			Dir:           path.Join(getCwd(t), "datasource-py"),
+			RunUpdateTest: true,
+		})
+
+	integration.ProgramTest(t, &test)
+}
+
+func TestAccAciMulti(t *testing.T) {
+	skipIfShort(t)
+	test := getJSBaseOptions(t).
+		With(integration.ProgramTestOptions{
+			Dir:           path.Join(getCwd(t), "aci-multi"),
+			RunUpdateTest: true,
+		})
+
+	integration.ProgramTest(t, &test)
+}
+
+func TestAccAciVolumeMount(t *testing.T) {
+	skipIfShort(t)
+	test := getJSBaseOptions(t).
+		With(integration.ProgramTestOptions{
+			Dir:           path.Join(getCwd(t), "aci-volume-mount"),
+			RunUpdateTest: true,
+		})
+
+	integration.ProgramTest(t, &test)
+}
+
+func TestAccCosmosDb(t *testing.T) {
+	skipIfShort(t)
+	test := getJSBaseOptions(t).
+		With(integration.ProgramTestOptions{
+			Dir:           path.Join(getCwd(t), "cosmosdb"),
+			RunUpdateTest: true,
+		})
+
+	integration.ProgramTest(t, &test)
+}
+
+func TestAccLoadbalancer(t *testing.T) {
+	skipIfShort(t)
+	test := getJSBaseOptions(t).
+		With(integration.ProgramTestOptions{
+			Dir:           path.Join(getCwd(t), "loadbalancer"),
+			RunUpdateTest: true,
+		})
+
+	integration.ProgramTest(t, &test)
+}
+
+func TestAccMultiCallback(t *testing.T) {
+	skipIfShort(t)
+	test := getJSBaseOptions(t).
+		With(integration.ProgramTestOptions{
+			Dir:           path.Join(getCwd(t), "multi-callback-all"),
+			RunUpdateTest: true,
+		})
+
+	integration.ProgramTest(t, &test)
+}
+
+func skipIfShort(t *testing.T) {
+	if testing.Short() {
+		t.Skip("skipping long-running test in short mode")
 	}
+}
+
+func getEnviron(t *testing.T) string {
+	env := os.Getenv("ARM_ENVIRONMENT")
+	if env == "" {
+		t.Skipf("Skipping test due to missing ARM_ENVIRONMENT environment variable")
+	}
+
+	return env
+}
+
+func getLocation(t *testing.T) string {
 	azureLocation := os.Getenv("ARM_LOCATION")
 	if azureLocation == "" {
 		azureLocation = "westus"
 		fmt.Println("Defaulting ARM_LOCATION to 'westus'.  You can override using the ARM_LOCATION variable")
 	}
+
+	return azureLocation
+}
+
+func getCwd(t *testing.T) string {
 	cwd, err := os.Getwd()
-	if !assert.NoError(t, err, "expected a valid working directory: %v", err) {
-		return
+	if err != nil {
+		t.Error("expected a valid working directory", err)
 	}
 
-	// base options shared amongst all tests.
-	base := integration.ProgramTestOptions{
+	return cwd
+}
+
+func getBaseOptions(t *testing.T) integration.ProgramTestOptions {
+	environ := getEnviron(t)
+	azureLocation := getLocation(t)
+	return integration.ProgramTestOptions{
+		ExpectRefreshChanges: true,
 		Config: map[string]string{
 			"azure:environment": environ,
 			"azure:location":    azureLocation,
 		},
 	}
+}
 
-	jsBase := base.With(integration.ProgramTestOptions{
+func getJSBaseOptions(t *testing.T) integration.ProgramTestOptions {
+	base := getBaseOptions(t)
+	baseJS := base.With(integration.ProgramTestOptions{
 		Dependencies: []string{
 			"@pulumi/azure",
 		},
 	})
 
+	return baseJS
+}
+
+func getPythonBaseOptions(t *testing.T) integration.ProgramTestOptions {
+	base := getBaseOptions(t)
 	pythonBase := base.With(integration.ProgramTestOptions{
 		Dependencies: []string{
 			filepath.Join("..", "sdk", "python", "bin"),
 		},
 	})
 
-	shortTests := []integration.ProgramTestOptions{
-		jsBase.With(integration.ProgramTestOptions{Dir: path.Join(cwd, "minimal")}),
-		jsBase.With(integration.ProgramTestOptions{
-			Dir: path.Join(cwd, "eventgrid"),
-		}),
-		jsBase.With(integration.ProgramTestOptions{
-			Dir:           path.Join(cwd, "eventhub"),
-			RunUpdateTest: true,
-			// work around https://github.com/terraform-providers/terraform-provider-azurerm/issues/4598
-			AllowEmptyPreviewChanges: true,
-			AllowEmptyUpdateChanges:  true,
-		}),
-		jsBase.With(integration.ProgramTestOptions{
-			Dir:           path.Join(cwd, "http-external"),
-			RunUpdateTest: true,
-		}),
-		jsBase.With(integration.ProgramTestOptions{
-			Dir:           path.Join(cwd, "http-multi"),
-			RunUpdateTest: true,
-			// work around https://github.com/terraform-providers/terraform-provider-azurerm/issues/4598
-			AllowEmptyPreviewChanges: true,
-			AllowEmptyUpdateChanges:  true,
-		}),
-		jsBase.With(integration.ProgramTestOptions{
-			Dir:           path.Join(cwd, "iot"),
-			RunUpdateTest: true,
-		}),
-		jsBase.With(integration.ProgramTestOptions{
-			Dir:           path.Join(cwd, "queue"),
-			RunUpdateTest: true,
-		}),
-		//jsBase.With(integration.ProgramTestOptions{
-		//	Dir:           path.Join(cwd, "table"),
-		//	RunUpdateTest: true,
-		//}),
-		jsBase.With(integration.ProgramTestOptions{
-			Dir:           path.Join(cwd, "timer"),
-			RunUpdateTest: true,
-		}),
-		jsBase.With(integration.ProgramTestOptions{
-			Dir:           path.Join(cwd, "topic"),
-			RunUpdateTest: true,
-		}),
-		jsBase.With(integration.ProgramTestOptions{
-			Dir:                  path.Join(cwd, "webserver"),
-			RunUpdateTest:        true,
-			ExpectRefreshChanges: true,
-		}),
-		jsBase.With(integration.ProgramTestOptions{
-			Dir:           path.Join(cwd, "http"),
-			RunUpdateTest: true,
-			ExtraRuntimeValidation: validateAPITest(func(body string) {
-				assert.Equal(t, body, "Hello World!")
-			}),
-		}),
-		jsBase.With(integration.ProgramTestOptions{
-			Dir:           path.Join(cwd, "blob"),
-			RunUpdateTest: true,
-			ExtraRuntimeValidation: validateAPITest(func(body string) {
-				assert.Equal(t, body, "A File from Blob Storage")
-			}),
-		}),
-		jsBase.With(integration.ProgramTestOptions{
-			Dir:           path.Join(cwd, "msi-renamed-to-authorization"),
-			RunUpdateTest: true,
-			EditDirs: []integration.EditDir{
-				{
-					Dir:             "step2",
-					Additive:        true,
-					ExpectNoChanges: true,
-				},
-			},
-		}),
-		jsBase.With(integration.ProgramTestOptions{
-			Dir:           path.Join(cwd, "servicebus-migration-test"),
-			RunUpdateTest: true,
-			EditDirs: []integration.EditDir{
-				{
-					Dir:             "step2",
-					Additive:        true,
-					ExpectNoChanges: true,
-				},
-			},
-		}),
-		pythonBase.With(integration.ProgramTestOptions{Dir: path.Join(cwd, "minimal-py")}),
-		pythonBase.With(integration.ProgramTestOptions{
-			Dir:           path.Join(cwd, "eventhub-py"),
-			RunUpdateTest: true,
-			// work around https://github.com/terraform-providers/terraform-provider-azurerm/issues/4598
-			AllowEmptyPreviewChanges: true,
-			AllowEmptyUpdateChanges:  true,
-		}),
-		pythonBase.With(integration.ProgramTestOptions{
-			Dir:           path.Join(cwd, "datasource-py"),
-			RunUpdateTest: true,
-		}),
-	}
-
-	longTests := []integration.ProgramTestOptions{
-		jsBase.With(integration.ProgramTestOptions{
-			Dir:           path.Join(cwd, "aci-multi"),
-			RunUpdateTest: true,
-		}),
-		jsBase.With(integration.ProgramTestOptions{
-			Dir:           path.Join(cwd, "aci-volume-mount"),
-			RunUpdateTest: true,
-		}),
-		jsBase.With(integration.ProgramTestOptions{
-			Dir:           path.Join(cwd, "cosmosdb"),
-			RunUpdateTest: true,
-		}),
-		jsBase.With(integration.ProgramTestOptions{
-			Dir:           path.Join(cwd, "loadbalancer"),
-			RunUpdateTest: true,
-		}),
-		jsBase.With(integration.ProgramTestOptions{
-			Dir:           path.Join(cwd, "multi-callback-all"),
-			RunUpdateTest: true,
-		}),
-	}
-
-	tests := shortTests
-	if !testing.Short() {
-		tests = append(tests, longTests...)
-	}
-
-	for _, ex := range tests {
-		example := ex.With(integration.ProgramTestOptions{
-			// TODO[pulumi/pulumi#1900]: This should be the default value, every test we have causes some sort of
-			// change during a `pulumi refresh` for reasons outside our control.
-			ExpectRefreshChanges: true,
-		})
-		t.Run(example.Dir, func(t *testing.T) {
-			integration.ProgramTest(t, &example)
-		})
-	}
-}
-
-func createEditDir(dir string) integration.EditDir {
-	return integration.EditDir{Dir: dir, ExtraRuntimeValidation: nil}
+	return pythonBase
 }
 
 func validateAPITest(isValid func(body string)) func(t *testing.T, stack integration.RuntimeValidationStackInfo) {
