a9f77d4b0e9d04944cab4299a807c3594e84c880
Thomas Kappler <tkappler@pulumi.com>
2024-10-07 14:12:14 +0200

    Ignore golangci-lint's misspell for hdinsight

--- provider/provider_yaml_test.go
+++ provider/provider_yaml_test.go
@@ -95,6 +95,7 @@ func Test_containerservice_k8scluster(t *testing.T) {
 		optproviderupgrade.NewSourcePath(filepath.Join("test-programs", "containerservice_k8scluster", "v6")))
 }
 
+//nolint:misspell
 func Test_hdinsight(t *testing.T) {
 	test(t, filepath.Join("test-programs", "hdinsight"),
 		optproviderupgrade.NewSourcePath(filepath.Join("test-programs", "hdinsight", "v6")))
