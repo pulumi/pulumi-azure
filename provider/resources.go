8c22465a24fe065b3dc4074a930a5cc56c73009d
Ian Wahbe <me@iwahbe.com>
2024-11-14 08:35:43 +0100

    Add display name (#2630)

--- provider/resources.go
+++ provider/resources.go
@@ -520,6 +520,7 @@ func Provider() tfbridge.ProviderInfo {
 	prov := tfbridge.ProviderInfo{
 		P:                p,
 		Name:             "azurerm",
+		DisplayName:      "Azure",
 		Description:      "A Pulumi package for creating and managing Microsoft Azure cloud resources, based on the Terraform azurerm provider. We recommend using the [Azure Native provider](https://github.com/pulumi/pulumi-azure-native) to provision Azure infrastructure. Azure Native provides complete coverage of Azure resources and same-day access to new resources and resource updates.",
 		Keywords:         []string{"pulumi", "azure"},
 		Homepage:         "https://pulumi.io",
